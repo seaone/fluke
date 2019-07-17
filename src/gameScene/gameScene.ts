a09377237dcfe1a3fe8c9062c3bd9ea953b97632
Eugene Sanzhiev <eugene.sanzhiev@team.wrike.com>
2019-07-17 14:34:56 +0300

    font-style

--- src/gameScene/gameScene.ts
+++ src/gameScene/gameScene.ts
@@ -6,8 +6,12 @@ import {gameOptions} from "./gameOptions";
 export class GameScene extends Phaser.Scene {
   private fluke: Fluke;
   private platform: Platform;
-  private score = 0;
+  private score: number = 0;
   private scoreText: Phaser.GameObjects.Text;
+  private fontStyle = {
+    font: "16px 'PressStart'",
+    fill: "#fff",
+  };
 
   constructor() {
     super({
@@ -25,14 +29,19 @@ export class GameScene extends Phaser.Scene {
     this.platform = new Platform(this);
     this.fluke = new Fluke(this);
     this.physics.add.collider(this.fluke.sprite, this.platform.platformGroup.getChildren());
-    this.scoreText = this.add.text(24, 16, `score: ${this.score}`, { fontSize: '32px', fill: '#fff' });
+    this.scoreText = this.add.text(24, 16, `score: ${this.score}`, this.fontStyle);
     this.add.image(+this.game.config.width / 2, +this.game.config.height / 4, 'mainTitle');
   }
 
   update(): void {
-    this.score++;
     this.fluke.update();
     this.platform.update();
-    this.scoreText.setText(`score: ${this.score}`);
+
+    if (gameOptions.isStarted) {
+      this.score++;
+      this.scoreText.setText(`score: ${this.score}`);
+    } else {
+      this.score = 0;
+    }
   }
 }
